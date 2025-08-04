4deb74dc1fe9373022b48e34610842cce1534e4a
SongKangWoon <zzxz666@gmail.com>
2025-08-04 22:38:47 +0900

    SEO(검색엔진최적화)에 부합하는 URL설정

--- blog/views.py
+++ blog/views.py
@@ -8,14 +8,19 @@ def post_list(request):
 
     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request, id):
-    # try:
-    #
-    #     post = Post.published.get(id=id) # id로 가지고옴
-    #
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
+# def post_detail(request, id):
+#     # try:
+#     #
+#     #     post = Post.published.get(id=id) # id로 가지고옴
+#     #
+#     # except Post.DoesNotExist:
+#     #     raise Http404("No Post found.")
+#
+#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED) # 매개변수와 일치하는 객체를 찾거나 해당 객체가 없으면 http404(찾을수없음) 예외를 발생시킴
+#
+#     return render(request,'blog/post/detail.html',{'post':post})
 
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED) # 매개변수와 일치하는 객체를 찾거나 해당 객체가 없으면 http404(찾을수없음) 예외를 발생시킴
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug =post, publish__year = year, publish__month = month, publish__day = day)
 
     return render(request,'blog/post/detail.html',{'post':post})
